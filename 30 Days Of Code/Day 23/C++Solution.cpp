79dbbe9aac2f6da8adce8db755768074f7c070ab
berkeodaci <66134122+berkeodaci@users.noreply.github.com>
2020-06-27 13:41:02 +0300

    Add files via upload

---
+++ 30 Days Of Code/Day 23/C++Solution.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cstddef>
+#include <queue>
+#include <string>
+#include <cstdlib>
+
+using namespace std;	
+class Node{
+    public:
+        int data;
+        Node *left,*right;
+        Node(int d){
+            data=d;
+            left=right=NULL;
+        }
+};
+class Solution{
+    public:
+  		Node* insert(Node* root, int data){
+            if(root==NULL){
+                return new Node(data);
+            }
+            else{
+                Node* cur;
+                if(data<=root->data){
+                    cur=insert(root->left,data);
+                    root->left=cur;
+                }
+                else{
+                   cur=insert(root->right,data);
+                   root->right=cur;
+                 }           
+           return root;
+           }
+        }
+
+	void levelOrder(Node * root){
+      //Write your code here
+        queue<Node *> result;
+        Node* point = root;
+        while(point != NULL)
+        {
+            cout<<point->data<<" ";
+            if(point->left != NULL)
+            {
+                result.push(point->left);
+            }
+            if(point-> right != NULL)
+            {
+                result.push(point->right);
+            }
+            if(!result.empty())
+            {
+                point=result.front();
+                result.pop();
+            }
+            else
+            {
+                point = NULL;
+            }
+        }
+  
+	}
+
+};//End of Solution
+int main(){
+    Solution myTree;
+    Node* root=NULL;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        root= myTree.insert(root,data);
+    }
+    myTree.levelOrder(root);
+    return 0;
+}
